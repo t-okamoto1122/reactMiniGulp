343e00cc6b4e25012e86be0ec94f08a5493bbe31
t-okamoto1122 <t-okamoto@begood-tech.com>
2016-04-14 10:54:57 +0900

    react input form

--- public/helloworld.jsx
+++ public/helloworld.jsx
@@ -1,10 +1,42 @@
-var HelloMessage = React.createClass({
-    render: function() {
-        return <div>Hello {this.props.name}!</div>;
-    }
+
+//React.createClass()でコンポーネントをつくり、Render()で指定したDOM要素にそのコンポーネントを
+//レンダリングする。
+
+
+//React.createClass()でこんぽーねんとをつくり、Render()で指定したDOM要素にそのコンポーネントを
+//レンダリングする。
+
+// var HelloMessage = React.createClass({
+//     render: function() {
+//         return <div>Hello {this.props.name}!</div>;
+//     }
+// });
+//
+//
+// React.render(
+//     <HelloMessage name="World" />,
+//     document.getElementById('container')
+// );
+
+var App = React.createClass({
+  getInitialState: function() {
+    return { message: ""};
+  },
+  updateMessage: function(e) {
+    this.setState( { message: e.target.value });
+  },
+
+  render : function () {
+    return (
+      <div>
+        <input type="text" onChange={this.updateMessage} />
+        <p> {this.state.message}</p>
+      </div>
+    );
+  }
 });
 
-React.render(
-    <HelloMessage name="World" />,
-    document.getElementById('container')
-);
+ React.render(
+   <App />,
+   document.getElementById('app-container')
+ );
