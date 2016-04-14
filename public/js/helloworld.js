6c30358d349159d772f40e69d1d4b66c2885a27d
t-okamoto1122 <t-okamoto@begood-tech.com>
2016-04-14 10:32:42 +0900

    add comment

--- public/js/helloworld.js
+++ public/js/helloworld.js
@@ -1,10 +1,15 @@
+//React.createClass()でコンポーネントをつくり、Render()で指定したDOM要素にそのコンポーネントを
+//レンダリングする。
+
 var HelloMessage = React.createClass({displayName: "HelloMessage",
     render: function() {
+      //Hello~~をつくるHelloMessageコンポーネントをつくる
         return React.createElement("div", null, "Hello ", this.props.name, "!");
     }
 });
 
 React.render(
+  //上のHelloMessageコンポーネントを呼び出し
     React.createElement(HelloMessage, {name: "World"}),
     document.getElementById('container')
 );
